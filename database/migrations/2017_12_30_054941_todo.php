0b24fe59dec0a7040160342e34411bf9151c8dd1
archivescode <archivescode@gmail.com>
2017-12-30 12:55:02 +0700

    add dragula and todo

---
+++ database/migrations/2017_12_30_054941_todo.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Todo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul', 20)->unique();
+            $table->string('isi', 255);
+            $table->enum('status', ['belum', 'sedang', 'review', 'selese']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('todo');
+    }
+}
